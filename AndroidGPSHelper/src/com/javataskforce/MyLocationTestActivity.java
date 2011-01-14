7c8e928ee1c922362aed36f602ecc3823890c95b
Adarsh <pandey.adarsh147@gmail.com>
2011-01-15 04:24:50 +0530

    Main application init

---
+++ AndroidGPSHelper/src/com/javataskforce/MyLocationTestActivity.java
@@ -0,0 +1,13 @@
+package com.javataskforce;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyLocationTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
